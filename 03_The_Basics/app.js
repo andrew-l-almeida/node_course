6831973b39b6e4c0c4cda653e68aaf730580b02f
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-10 20:35:50 -0300

    Module 03 Final

--- 03_The_Basics/app.js
+++ 03_The_Basics/app.js
@@ -1,47 +1,7 @@
 const http = require('http')
-const fs = require('fs')
 
+const routes = require(`./routes`)
 
-
-const server = http.createServer(function(req, res){
-    const url = req.url
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>')
-        res.write('<head>')
-        res.write('</head>')
-        res.write('<body>')
-        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
-        res.write('</body>')
-        res.write('</html>')
-        return res.end()
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = []
-        req.on('data', (chunk) =>{
-            body.push(chunk)
-        });
-        req.on('end', ()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1]
-            fs.writeFile('message.txt', message, err =>{
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            })
-        })
-    }
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head>')
-    res.write('</head>')
-    res.write('<body>')
-    res.write('<h1>')
-    res.write('Andrew')
-    res.write('</h1>')
-    res.write('</body>')
-    res.write('</html>')
-    res.end()
-}); //Create a server 
+const server = http.createServer(routes)
 
 server.listen(3000)
---
+++ 03_The_Basics/routes.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+
+const requestHandles = (req, res) =>{
+    const url = req.url
+    const method = req.method
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head>')
+        res.write('</head>')
+        res.write('<body>')
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = []
+        req.on('data', (chunk) =>{
+            body.push(chunk)
+        });
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+
+            const message = parsedBody.split('=')[1]
+
+            fs.writeFile('message.txt', message, err =>{
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                
+            });
+        })
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head>')
+    res.write('</head>')
+    res.write('<body>')
+    res.write('<h1>')
+    res.write('Andrew')
+    res.write('</h1>')
+    res.write('</body>')
+    res.write('</html>')
+    return res.end()   
+}
+module.exports = requestHandles
+
