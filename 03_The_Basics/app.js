d9754889c67a1f750c38e7fb94c4f121071426ef
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-10 14:32:51 -0300

    Added Buffer to parse data to txt file

--- 03_The_Basics/app.js
+++ 03_The_Basics/app.js
@@ -1,9 +1,11 @@
 const http = require('http')
+const fs = require('fs')
 
 
 
 const server = http.createServer(function(req, res){
     const url = req.url
+    const method = req.method;
     if(url === '/'){
         res.write('<html>')
         res.write('<head>')
@@ -12,6 +14,20 @@ const server = http.createServer(function(req, res){
         res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
         res.write('</body>')
         res.write('</html>')
+        return res.end()
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = []
+        req.on('data', (chunk) =>{
+            body.push(chunk)
+        });
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+        res.statusCode = 302
+        res.setHeader('Location', '/')
         return res.end()
     }
     res.setHeader('Content-Type', 'text/html')
