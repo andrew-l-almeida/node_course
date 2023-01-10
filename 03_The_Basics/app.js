009a8a810c999c1382e6bbafc2e51bb72198e771
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-10 13:47:32 -0300

    Added form, and /message rout

--- 03_The_Basics/app.js
+++ 03_The_Basics/app.js
@@ -3,6 +3,17 @@ const http = require('http')
 
 
 const server = http.createServer(function(req, res){
+    const url = req.url
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head>')
+        res.write('</head>')
+        res.write('<body>')
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head>')
@@ -13,6 +24,7 @@ const server = http.createServer(function(req, res){
     res.write('</h1>')
     res.write('</body>')
     res.write('</html>')
+    res.end()
 }); //Create a server 
 
 server.listen(3000)
