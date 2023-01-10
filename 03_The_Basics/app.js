9d538de2205202276e09e5922fd5c70bb3275344
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-10 10:33:55 -0300

    Rearranging Folders

---
+++ 03_The_Basics/app.js
@@ -0,0 +1,16 @@
+
+/**core modules
+ * http -> launch a server, send requests
+ * https -> launch a SSL server
+ * fs
+ * path
+ * os
+ */
+
+const http = require('http')
+
+function rqListener(req, res){
+
+}
+
+http.createServer(); //Create a server 
