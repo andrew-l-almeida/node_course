4d79650bed06f3a0a6f5a59d0478974374443fa5
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-10 23:00:30 -0300

    Added body parser to the project

--- 05_Work_With_Express/app.js
+++ 05_Work_With_Express/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use('/products', (req, res, next) =>{
+app.use(bodyParser.urlencoded({extended: false})) //Middleware to parse the body requests
+
+app.use('/add-products', (req, res, next) =>{
     console.log('In another middleware')
-    res.send('<h1>Products</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
+})
+
+app.use('/product', (req, res) =>{
+    console.log(req.body)
+    res.redirect('/')
 })
 app.use('/', (req, res, next) =>{
     console.log('In another middleware')
