b941a186e5240c9261c38d03d0a173f53b5c420b
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-11 14:52:10 -0300

    Added 404 page

--- 05_Work_With_Express/app.js
+++ 05_Work_With_Express/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
@@ -11,8 +12,8 @@ app.use(bodyParser.urlencoded({extended: false})) //Middleware to parse the body
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use((req, res) =>{
-    res.send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
-
+console.log('listin in: localhost:3000')
 app.listen(3000)
