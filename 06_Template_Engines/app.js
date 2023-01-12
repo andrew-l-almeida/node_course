b5d1e85568a5632c8c7b42377c20a8e1f8a1353c
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-11 22:39:15 -0300

    Starting to work with pug template engine

--- 06_Template_Engines/app.js
+++ 06_Template_Engines/app.js
@@ -3,7 +3,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
