3238da23a3518ac1d7fa00a0e758a32bb5246c9e
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-13 08:57:53 -0300

    Starting to work with EJS

--- 06_Template_Engines/app.js
+++ 06_Template_Engines/app.js
@@ -6,8 +6,7 @@ const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutDir:'views/layouts/', defaultLayout:'main-layout', extname:'hbs'}))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminData = require('./routes/admin');
