f50d600ff72624a4a7297db95310bbdae61f5ea1
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-15 00:36:38 -0300

    Added static publics files

--- 00_Exercises/Exercise_04/EJS/app.js
+++ 00_Exercises/Exercise_04/EJS/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const routes = require('./routes/routes')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
