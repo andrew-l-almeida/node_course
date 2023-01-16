ceeddbecd51a3925c9851c54e038af4edfef2075
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 13:10:23 -0300

    Added static folder and static files

--- 00_Exercises/Exercise_04/PUG/app.js
+++ 00_Exercises/Exercise_04/PUG/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const routes = require('./routes/routes')
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'pug')
 app.set('views', 'views')
