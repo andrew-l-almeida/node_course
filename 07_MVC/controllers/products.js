81c22bc893ede1e04a1e1ff8e54b977b9fea0c53
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 14:31:25 -0300

    Added models folder and product models

--- 07_MVC/controllers/products.js
+++ 07_MVC/controllers/products.js
@@ -1,14 +1,16 @@
-const products = [];
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {title: 'Add Product', path:'/admin/add-product', activeAddProduct: true, formsCSS: true, productCSS: true})
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/');
 }
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = (req, res, next) => { 
+    const products = Product.fetchAll()
     res.render('shop', {prods: products, title: 'Shop', path: '/', hasProducts:products.length > 0, activeShop: true, productCSS: true})
 }
