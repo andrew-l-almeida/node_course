13ad9bd5c038b9da016fb752ed023f94504612f4
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 13:06:53 -0300

    Added route post to /user, added id to users and form to main.pug

--- 00_Exercises/Exercise_04/PUG/app.js
+++ 00_Exercises/Exercise_04/PUG/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes/routes')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}))
+
 app.set('view engine', 'pug')
 app.set('views', 'views')
 
--- 00_Exercises/Exercise_04/PUG/routes/routes.js
+++ 00_Exercises/Exercise_04/PUG/routes/routes.js
@@ -2,10 +2,16 @@ const express = require('express')
 
 const routes = express.Router();
 
+const users = []
+
 routes.get('/', (req, res) =>{
     res.render('main', {title: 'Add users'})
 })
 routes.get('/users', (req, res) =>{
     res.render('users', {title: 'Users'})
+})
+routes.post('/users', (req, res) =>{
+    users.push({name: req.body.name, id: users.length})
+    res.render('users', {users})
 })
 module.exports = routes;
