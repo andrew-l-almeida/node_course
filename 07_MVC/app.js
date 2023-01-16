7ff74bf803a4018fcf346d41a5973552b4712470
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 13:27:35 -0300

    Starting to work with mvc

---
+++ 07_MVC/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars')
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {title: 'Page not Found', path: null})
+});
+
+app.listen(3000);
