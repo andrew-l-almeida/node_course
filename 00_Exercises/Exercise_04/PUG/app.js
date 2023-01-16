5d4c15a16ea141df86c69a0d15dcc8ea1f7ca4c3
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 11:13:44 -0300

    Added pug package, and setted view engine and view file

--- 00_Exercises/Exercise_04/PUG/app.js
+++ 00_Exercises/Exercise_04/PUG/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+
 const routes = require('./routes/routes')
 
 const app = express();
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 app.use(routes)
 
+app.use('/', (req, res) =>{
+    res.render('404', {title: 'Page not found'})
+})
 app.listen(3000)
