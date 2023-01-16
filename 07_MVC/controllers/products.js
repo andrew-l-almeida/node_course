b293c318e711f4891980ee11e440c7895d211a0d
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 16:03:24 -0300

    Reading json file to get products

--- 07_MVC/controllers/products.js
+++ 07_MVC/controllers/products.js
@@ -11,6 +11,7 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => { 
-    const products = Product.fetchAll()
-    res.render('shop', {prods: products, title: 'Shop', path: '/', hasProducts:products.length > 0, activeShop: true, productCSS: true})
+    Product.fetchAll((products) =>{
+        res.render('shop', {prods: products, title: 'Shop', path: '/', hasProducts:products.length > 0, activeShop: true, productCSS: true})
+    })
 }
--- 07_MVC/models/product.js
+++ 07_MVC/models/product.js
@@ -1,4 +1,5 @@
-const products = []
+const path = require('path')
+const fs = require('fs')
 
 module.exports = class Product {
     constructor(title){
@@ -6,10 +7,26 @@ module.exports = class Product {
     }
 
     save(){
-        products.push(this)
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) =>{
+            let products = []
+            if(!err){
+                 products = JSON.parse(fileContent);
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) =>{
+                console.log(err)
+            })
+        });
     }
 
-    static fetchAll(){
-        return products
+    static fetchAll(cb){
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) =>{
+            if(err){
+                cb([])
+            }
+            cb(JSON.parse(fileContent))
+        })
     }
 }
