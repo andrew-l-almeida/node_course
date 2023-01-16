ae7e891f8748d12c89118ca89b62fd8e9a262718
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 16:13:58 -0300

    Refatoring the code

--- 07_MVC/models/product.js
+++ 07_MVC/models/product.js
@@ -1,18 +1,26 @@
 const path = require('path')
 const fs = require('fs')
 
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+const getProductsFromFile = (cb) =>{
+        fs.readFile(p, (err, fileContent) =>{
+            if(err){
+                cb([])
+            }else{
+                cb(JSON.parse(fileContent))
+            }
+            
+        })
+}
+
 module.exports = class Product {
     constructor(title){
         this.title = title
     }
 
     save(){
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) =>{
-            let products = []
-            if(!err){
-                 products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products =>{
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) =>{
                 console.log(err)
@@ -21,12 +29,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb){
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) =>{
-            if(err){
-                cb([])
-            }
-            cb(JSON.parse(fileContent))
-        })
+        getProductsFromFile(cb)
     }
 }
