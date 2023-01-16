33269225774819bf5830610bd104a01dc8decbaa
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-16 11:19:39 -0300

    Created routes and pug's files

--- 00_Exercises/Exercise_04/PUG/routes/routes.js
+++ 00_Exercises/Exercise_04/PUG/routes/routes.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const routes = express.Router();
 
+routes.get('/', (req, res) =>{
+    res.render('main', {title: 'Add users'})
+})
+routes.get('/users', (req, res) =>{
+    res.render('users', {title: 'Users'})
+})
 module.exports = routes;
