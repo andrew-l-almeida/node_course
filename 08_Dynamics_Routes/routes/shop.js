e15c3fbdb83a12964d991810ded2f6e2fc8dd6ad
Andrew Almeida <andrew.l.almeida@outook.com>
2023-01-17 09:59:53 -0300

    Added /products/:productId route and a controller to deal with the Id of the product

--- 08_Dynamics_Routes/routes/shop.js
+++ 08_Dynamics_Routes/routes/shop.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts)
+router.get('/products/:productId', shopController.getProduct) // ":" means that we expect a variable in the URL
 router.get('/cart', shopController.getCart)
 router.get('/orders', shopController.getOrders)
 router.get('/checkout', shopController.getCheckkout)
